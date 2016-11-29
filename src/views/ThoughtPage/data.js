0e6f924fb6d006709c350cbf11f1eae38d5c2239
kitze <kristijan.mkd@gmail.com>
2016-11-29 17:47:46 +0100

    🛠 use GraphQL fragments for Thought

--- src/views/ThoughtPage/data.js
+++ src/views/ThoughtPage/data.js
@@ -1,15 +1,13 @@
 import gql from 'graphql-tag';
+import {ThoughtFragment} from 'gql/fragments';
 
 export const ThoughtQuery = gql`query($slug: String) {
     allThoughts(filter:{slug:$slug}){
-        slug
-        id
-        title
-        content
-        tags
-        createdAt
+        ...Thought
     }
-}`;
+}
+${ThoughtFragment}
+`;
 
 export const options = ({store}) => {
   const {slug} = store.router.params;
