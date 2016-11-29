ba8d6af21f18467e8b003c5b749c33b47b330a12
kitze <kristijan.mkd@gmail.com>
2016-11-29 14:29:34 +0100

    🛠remove stores

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,12 +1,8 @@
 import AppStore from 'stores/global/app-store.mobx';
-import NewThoughtPageStore from 'stores/views/new-thought-page-store.mobx';
-import AuthStore from 'stores/global/auth-store.mobx';
 import {RouterStore} from 'mobx-router';
 
 const store = {
   app: new AppStore(),
-  auth: new AuthStore(),
-  newThoughtPage: new NewThoughtPageStore(),
   router: new RouterStore(),
 };
 
